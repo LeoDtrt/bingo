d7d769e261e24e5020605c980d213b92a2aa3e18
Léo Dutertre <ldutertreladuree@gmail.com>
2025-04-29 19:32:55 +0200

    mef

--- src/app.py
+++ src/app.py
@@ -6,26 +6,52 @@ app = dash.Dash(__name__)
 server = app.server
 
 # Liste fixe d’activités
-activites = ["Jeu de société", "Karaoké", "Dessiner", "Quiz", "Danse", "Mime"]
+activites = [
+    """faire une imitation de Max""",
+    """improviser un petit discours faisant l'éloge de Max""",
+    """boire un shot""",
+    """porter la perruque de la honte pendant 20 minutes""",
+    """faire 10 pompes""",
+    """faire 10 burpees""",
+    """faire 10 squats""",
+    """faire 1 minutes de planche""",
+    """aller être dans le bain nordique dans moins de 5 minutes""",
+    """faire le poirier sur le tapis magique""",
+    """faire 5 roulades sur le tapis magique""",
+    """s'adresser aux vainqueurs en les nommant "Master of the puppets" pendant une heure""",
+    """s'adresser aux vainqueurs en les nommant "Oh capitaine mon capitaine" pendant une heure""",
+    """s'adresser aux vainqueurs en les nommant "Numéro Uno" pendant une heure""",
+    """manger une dragée surprise de Bertie Crochue""",
+    """demander l’avis de l’audience pour son action"""
+]
+
+
+
+
 
 app.layout = html.Div([
-    html.H1("Tirage au sort : nom & activité"),
+    html.Div([
+        html.H1("Bingo : Tirage au sort"),
 
-    html.H2("Ajouter un nom :"),
-    dcc.Input(id='input-nom', type='text', placeholder='Entrez un nom'),
-    html.Button('Ajouter', id='btn-ajouter', n_clicks=0),
-    html.Button('Réinitialiser', id='btn-reset', n_clicks=0, style={'marginLeft': '10px'}),
+        html.H2("Ajouter un nom :"),
+        dcc.Input(id='input-nom', type='text', placeholder='Entrez un nom', className='input-nom'),
+        html.Button('Ajouter', id='btn-ajouter', n_clicks=0, className='dash-button'),
+        html.Button('Réinitialiser', id='btn-reset', n_clicks=0, className='dash-button'),
 
-    html.H3("Liste des noms :"),
-    html.Ul(id='liste-noms'),
+        html.H3("Liste des noms :"),
+        html.Ul(id='liste-noms'),
 
-    html.H4("Noms activés pour le tirage :"),
-    dcc.Checklist(id='checklist-noms', options=[], value=[], labelStyle={'display': 'block'}),
+        html.H3("Noms activés pour le tirage :"),
+        dcc.Checklist(id='checklist-noms', options=[], value=[], className='checklist-style'),
 
-    html.Button("Tirer un nom et une activité", id='btn-tirer', n_clicks=0),
-    html.Div(id='resultat-tirage', style={'marginTop': '20px', 'fontSize': '20px'}),
+        html.Button("Tirer un nom et une activité", id='btn-tirer', n_clicks=0, className='dash-button'),
+        html.Br(),
+        html.Br(),
+        html.Div(id='resultat-tirage'),
 
-    dcc.Store(id='noms-stockes', data=[])
+        dcc.Store(id='noms-stockes', data=[])        
+    ], className='container')
+
 ])
 
 # Ajouter un nom ou réinitialiser la liste
@@ -75,7 +101,7 @@ def tirer_nom(n_clicks, noms_actifs):
         return "Aucun nom actif pour le tirage !"
     nom = random.choice(noms_actifs)
     activite = random.choice(activites)
-    return f"{nom} va faire : {activite}"
+    return f"{nom} doit {activite}"
 
 if __name__ == '__main__':
     app.run_server(debug=True)
